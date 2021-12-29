de62423601e7e11850397d035ae8c0a15bfc68c2
himangshuj <hh@v3.cash>
2021-12-29 07:44:20 -0800

    Created ECR repositories for platform
    Signed-off-by: himangshuj <hh@v3.cash>

--- main.tf
+++ main.tf
@@ -20,3 +20,70 @@ provider "aws" {
   region  = var.aws_region
 }
 
+resource "aws_ecr_repository" "v3_cash_platform" {
+  name                 = "v3.cash/platform"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    Owner = var.aws_profile
+  }
+}
+
+resource "aws_iam_user" "v3_cash_platform_ci_user" {
+  name = "platform_ci@v3.cash"
+  tags = {
+    Owner = var.aws_profile
+  }
+}
+
+resource "aws_iam_policy" "v3_cash_platform_ci_policy" {
+  name        = "v3-cash-platform-ci-policy"
+  description = "iam policy to create platform container images"
+  tags = {
+    Owner = var.aws_profile
+  }
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+        Effect   = "Allow"
+        Resource = [aws_ecr_repository.v3_cash_platform.arn]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecr_base_policy" {
+  name        = "ecr-base-policy"
+  description = "iam policy to get authorization for ecr"
+  tags = {
+    Owner = var.aws_profile
+  }
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
--- variables.tf
+++ variables.tf
@@ -7,15 +7,3 @@ variable "aws_region" {
   type    = string
   default = "us-west-2"
 }
-
-variable "state_s3_bucket" {
-    type = string 
-}
-
-variable "state_s3_bucket_region" {
-    type = string 
-}
-
-variable "state_s3_bucket_key" {
-    type = string 
-}
